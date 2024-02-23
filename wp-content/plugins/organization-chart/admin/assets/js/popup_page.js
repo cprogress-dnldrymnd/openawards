3f146ae41c18b3c70076b26b6526ea32d39fc101
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-02-23 18:50:32 +0800

    Theme

--- wp-content/plugins/organization-chart/admin/assets/js/popup_page.js
+++
@@ -1,5 +0,0 @@
-function submitButton(value){
-	document.getElementById("adminForm").setAttribute("action",document.getElementById("adminForm").getAttribute("action")+"&task="+value);
-	jQuery('[disabled]').removeAttr('disabled');
-	document.getElementById("adminForm").submit();
-}
--- wp-content/plugins/yith-woocommerce-product-bundles/dist/cart-checkout-blocks/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wc-blocks-checkout'), 'version' => '4808dc381d01697422f2');
