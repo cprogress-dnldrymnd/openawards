0809e24bd8708c493b23016181046eadf80f0e52
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-02-23 19:47:06 +0800

    Update wp-bakery.php

--- includes/wp-bakery.php
+++ includes/wp-bakery.php
@@ -1,32 +1,36 @@
 <?php
 if (function_exists('vc_map')) {
 
-    $terms = get_terms(array(
-        'taxonomy'   => 'teams_category',
-        'hide_empty' => false,
-    ));
+    add_action('vc_before_init', 'vc_map_team_slider');
+    function vc_map_team_slider()
+    {
+        $terms = get_terms(array(
+            'taxonomy'   => 'teams_category',
+            'hide_empty' => false,
+        ));
 
-    $arr = array();
+        $arr = array();
 
-    foreach ($terms as $term) {
-        $arr[$term->term_id] = $term->name;
+        foreach ($terms as $term) {
+            $arr[$term->term_id] = $term->name;
+        }
+
+        vc_map(array(
+            "name" => "Team Slider",
+            "base" => "team_slider",
+            "category" => "Open Awards",
+            "params" => array(
+                array(
+                    "type" => "dropdown",
+                    "heading" => "Category",
+                    "param_name" => "category",
+                    "value" => $arr,
+                    "description" => "Select the team category you want to display."
+                ),
+            )
+
+        ));
     }
-
-    vc_map(array(
-        "name" => "Team Slider",
-        "base" => "team_slider",
-        "category" => "Open Awards",
-        "params" => array(
-            array(
-                "type" => "dropdown",
-                "heading" => "Category",
-                "param_name" => "category",
-                "value" => $terms,
-                "description" => "Select the team category you want to display."
-            ),
-        )
-
-    ));
 }
 
 function action_team_slider($atts)
