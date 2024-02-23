032340a7f28ab7d23a45a480f63823bebe448921
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-02-23 20:08:12 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -103,10 +103,6 @@
       slidesPerView: 4,
       spaceBetween: 40,
       loop: true,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
       navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
