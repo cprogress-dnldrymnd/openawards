036b73a18d27eb2ef37fb061464d6f12cddd5dfc
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-02-23 21:24:19 +0800

    css

--- footer.php
+++ footer.php
@@ -103,6 +103,20 @@
       slidesPerView: 4,
       spaceBetween: 40,
       loop: true,
+      breakpoints: {
+        575: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+        992: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+      },
       navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
