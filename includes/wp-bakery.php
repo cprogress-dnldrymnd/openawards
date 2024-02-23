0a19ce40b292596fbe235eb45b4ea578de2908a5
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-02-23 19:44:44 +0800

    Update wp-bakery.php

--- includes/wp-bakery.php
+++ includes/wp-bakery.php
@@ -1,31 +1,46 @@
 <?php
 if (function_exists('vc_map')) {
-    vc_map(array(
-        "name" => "Team Slider",
-        "base" => "team_slider",
-        "category" => "Open Awards",
-        "params" => array(
-            array(
-                "type" => "dropdown",
-                "heading" => "Category",
-                "param_name" => "category",
-                "value" => array(
 
+    add_action('vc_before_init', 'vc_map_team_slider');
+    function vc_map_team_slider()
+    {
+        $terms = get_terms(array(
+            'taxonomy'   => 'teams_category',
+            'hide_empty' => false,
+        ));
+
+        $arr = array();
+
+        foreach ($terms as $term) {
+            $arr[$term->term_id] = $term->name;
+        }
+
+        vc_map(array(
+            "name" => "Team Slider",
+            "base" => "team_slider",
+            "category" => "Open Awards",
+            "params" => array(
+                array(
+                    "type" => "dropdown",
+                    "heading" => "Category",
+                    "param_name" => "category",
+                    "value" => $terms,
+                    "description" => "Select the team category you want to display."
                 ),
-                "description" => "Select the team category you want to display."
-            ),
-        )
+            )
 
-    ));
+        ));
+    }
 }
 
-function action_team_slider($atts) {
+function action_team_slider($atts)
+{
     extract(shortcode_atts(array(
-       'button_text' => 'Click Me',
-       'button_color' => '#ff9900',
+        'button_text' => 'Click Me',
+        'button_color' => '#ff9900',
     ), $atts));
- 
+
     $output = 'xxxxxxxxxxxxxxxxx';
     return $output;
- }
- add_shortcode('team_slider', 'action_team_slider');
+}
+add_shortcode('team_slider', 'action_team_slider');
