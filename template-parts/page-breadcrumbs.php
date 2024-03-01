2424e7ff060ab8f65d67310986e6d76746df09bf
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-01 18:37:01 +0800

    css

--- template-parts/page-breadcrumbs.php
+++ template-parts/page-breadcrumbs.php
@@ -3,7 +3,13 @@
         <div class="container<?= container_width() ?>">
             <ol class="breadcrumb">
                 <li class="breadcrumb-item"><a href="<?= get_site_url() ?>">Home</a></li>
-                <li class="breadcrumb-item"><span><?php the_title() ?></span></li>
+                <?php if (is_single() || is_page()) { ?>
+                    <li class="breadcrumb-item"><span><?php the_title() ?></span></li>
+                <?php } ?>
+
+                <?php if (is_archive()) { ?>
+                    <li class="breadcrumb-item"><span><?php post_type_archive_title(false) ?></span></li>
+                <?php } ?>
             </ol>
         </div>
     </nav>
