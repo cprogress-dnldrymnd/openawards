ae903f465beb8d8b2c681505e19a7744fc1be8d5
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-02 23:47:24 +0800

    Update page-event.php

--- templates/page-event.php
+++ templates/page-event.php
@@ -94,13 +94,12 @@
                let myStrPrev = $prev.text();
                let monthPrev = myStrPrev.split(" ")[0];
                let yearPrev = myStrPrev.split(" ")[1];
-
-
-
                if (monthPrev != month && yearPrev != year) {
                     $new_text = '<div class="day-month"> ' + month + ' ' + year + ' </div><span class="list-date"><span class="day-num"> ' + daynum + ' </span><span class="day-word"> ' + dayword + ' </span></span>';
+                    console.log('not-equals');
                } else {
                     $new_text = '<span class="list-date"><span class="day-num"> ' + daynum + ' </span><span class="day-word"> ' + dayword + ' </span></span>';
+                    console.log('equals');
                }
                jQuery(this).attr('new_text', $new_text);
           });
