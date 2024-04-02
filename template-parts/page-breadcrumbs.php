c62628a9fe0c3c55a2a40ab71917cf9f0e760bbc
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-03 01:09:39 +0800

    Update page-breadcrumbs.php

--- template-parts/page-breadcrumbs.php
+++ template-parts/page-breadcrumbs.php
@@ -1,15 +1,20 @@
+<?php
+if (is_single() || is_page()) {
+    $title = get_the_title();
+} else if (is_archive()) {
+    $title = post_type_archive_title(false);
+} else if (is_home()) {
+    $title = 'Latest News';
+}
+?>
+
+
 <section class="breadcrumbs wocom">
     <nav aria-label="breadcrumb">
         <div class="container<?= container_width() ?>">
             <ol class="breadcrumb">
                 <li class="breadcrumb-item"><a href="<?= get_site_url() ?>">Home</a></li>
-                <?php if (is_single() || is_page()) { ?>
-                    <li class="breadcrumb-item"><span><?php the_title() ?></span></li>
-                <?php } ?>
-
-                <?php if (is_archive()) { ?>
-                    <li class="breadcrumb-item"><span><?php post_type_archive_title(false) ?></span></li>
-                <?php } ?>
+                <li class="breadcrumb-item"><span><?= $title ?></span></li>
             </ol>
         </div>
     </nav>
