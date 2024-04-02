a289cdfd7257f8817c00ee78aa502daa00115b8d
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-02 23:34:07 +0800

    Update page-event.php

--- templates/page-event.php
+++ templates/page-event.php
@@ -105,6 +105,7 @@
                     $new_text = jQuery('<span class="list-date"><span class="day-num"> ' + daynum + ' </span><span class="day-word"> ' + dayword + ' </span></span>');
                }
 
+               jQuery(this).html($new_text);
 
 
 
