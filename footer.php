7e9f266e9e0d769b1afac22c77054faa2aed70e3
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-03 13:50:27 +0800

    css

--- footer.php
+++ footer.php
@@ -63,7 +63,18 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        ...
+        <div class="row">
+          <div class="col-lg-4">
+            <div class="image-box">
+              <img src="">
+            </div>
+          </div>
+          <div class="col-lg-8">
+            <div class="description-box">
+
+            </div>
+          </div>
+        </div>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
--- js/main.js
+++ js/main.js
@@ -7,7 +7,9 @@ function team_modal_trigger() {
     jQuery('.team-modal-trigger').click(function (e) { 
         title = jQuery(this).attr('title');
         body = jQuery(this).attr('content');
-        jQuery('#teamModal .modal-body').html(body);
+        image = jQuery(this).attr('image');
+        jQuery('#teamModal .description-box').html(body);
+        jQuery('#teamModal .modal-title').html(title);
         e.preventDefault();
     });
 }
