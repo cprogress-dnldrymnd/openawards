157a3d6d0fde286582c593856e7caf2dc4cc5dd9
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-03 13:42:37 +0800

    css

--- footer.php
+++ footer.php
@@ -55,11 +55,11 @@
   </div>
 </footer>
 
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="teamModal" tabindex="-1" aria-labelledby="teamModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <h5 class="modal-title" id="teamModalLabel">Modal title</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
