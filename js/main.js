c4d3a7a83faade24a47caefdbdff3bf8cea41f46
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-03 13:53:33 +0800

    cs

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
-$(document).ready(function () {
-    
+jQuery(document).ready(function () {
+    footer_functions();
+    team_modal_trigger();
+    swiper_sliders();
 });
 
 
+function footer_functions() {
+    if (jQuery('.select-input, .select-input-wrapper select').length > 0) {
+        jQuery('.select-input, .select-input-wrapper select').select2();
+    }
+    jQuery('[data-toggle="offcanvas"]').on('click', function () {
+        jQuery('.offcanvas').toggleClass('open on');
+        jQuery('.offcanvas-collapse').toggleClass('open');
+        jQuery('body').toggleClass('lockscroll');
+    });
+}
+
 function team_modal_trigger() {
-    jQuery('.team-modal-trigger').click(function (e) { 
+    jQuery('.team-modal-trigger').click(function (e) {
         title = jQuery(this).attr('title');
         body = jQuery(this).attr('content');
         image = jQuery(this).attr('image');
@@ -12,5 +25,51 @@ function team_modal_trigger() {
         jQuery('#teamModal .description-box').html(body);
         jQuery('#teamModal .modal-title').html(title);
         e.preventDefault();
+    });
+}
+
+function swiper_sliders() {
+    var swiper = new Swiper(".mySwiperSlider", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+
+
+    $key = 1;
+    jQuery('.team-swiper').each(function (index, element) {
+
+        var $id = 'swiper' + $key;
+        var $id = new Swiper(".team-swiper", {
+            loop: true,
+            breakpoints: {
+                480: {
+                    slidesPerView: 2,
+                    spaceBetween: 20,
+                },
+                768: {
+                    slidesPerView: 3,
+                    spaceBetween: 30,
+                },
+                1200: {
+                    slidesPerView: 4,
+                    spaceBetween: 40,
+                },
+            },
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+        });
+
+        $key++;
     });
 }
