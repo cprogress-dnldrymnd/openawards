f70c86c99183315bc3da6ebf975e972e875dccb2
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-16 01:17:08 +0800

    Update single.php

--- single.php
+++ single.php
@@ -25,10 +25,58 @@ while (have_posts()) {
 
 			<section class="the-content">
 				<div class="container">
-					<div class="image-box">
+					<div class="image-box mb-5">
 						<img src="<?= get_the_post_thumbnail_url(get_the_ID(), 'large') ?>" alt="">
 					</div>
-					<?php the_content() ?>
+					<div class="content-box">
+						<?php the_content() ?>
+					</div>
+				</div>
+			</section>
+
+			<section class="post-slider">
+				<div class="container">
+					<div class="swiperPostSlider-holder position-relative swiper-button-style-1 p-0">
+						<div class="swiper swiperPostSlider ">
+							<div class="swiper-wrapper">
+								<?php while (have_posts()) {
+									the_post(); ?>
+									<div class="swiper-slide">
+										<div class="post-box post-box-slider">
+											<div class="row g-4 align-items-center">
+												<div class="col-12">
+													<div class="column-holder">
+														<div class="image-box">
+															<img src="<?= get_the_post_thumbnail_url(get_the_ID(), 'medium') ?>" alt="">
+														</div>
+													</div>
+												</div>
+												<div class="col-12">
+													<div class="column-holder">
+
+														<div class="content-box">
+															<div class="heading-box">
+																<h4><?php the_title() ?></h4>
+															</div>
+															<div class="description-box">
+																<?php the_excerpt() ?>
+															</div>
+															<div class="button-box">
+																<a href="<?php the_permalink() ?>">Read more</a>
+															</div>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								<?php } ?>
+							</div>
+						</div>
+						<div class="swiper-button-next swiper-button-next-post"></div>
+						<div class="swiper-button-prev swiper-button-next-post"></div>
+					</div>
+
 				</div>
 			</section>
 		</div><!-- #content .site-content -->
