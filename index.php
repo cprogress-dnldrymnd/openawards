34a033c695d96a8d82528d0a754e8b29fe8a4613
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-16 01:45:47 +0800

    css

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ get_header(); // This fxn gets the header.php file and renders it
 		<section class="post-slider">
 			<div class="container">
 				<div class="swiperPostSlider-holder position-relative swiper-button-style-1">
-					<div class="swiper swiperPostSlider ">
+					<div class="swiper swiperPostSlider bg-light">
 						<div class="swiper-wrapper">
 							<?php while (have_posts()) {
 								the_post(); ?>
