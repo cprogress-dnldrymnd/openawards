5fe78ea285cefe29e16536230464d679c53fa078
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-16 14:47:35 +0800

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -272,18 +272,12 @@ function template($atts)
 		)
 	);
 
-	$args = array(
-		'post_type' => 'templates',
-		'p' => $template_id
+	$content_post = get_post($template_id);
+	$content = $content_post->post_content;
+	$content = apply_filters('the_content', $content);
+	$content = str_replace(']]>', ']]&gt;', $content);
 
-	);
-	$query = new WP_Query($args);
-
-	while ($query->have_posts()) {
-		$query->the_post();
-		return do_shortcode(get_the_content());
-	}
-	wp_reset_postdata();
+	return $content;
 	
 }
 
