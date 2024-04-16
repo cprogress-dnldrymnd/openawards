3f32d22c1c0db27037df6ca9aa761f975ab41e20
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-16 14:20:06 +0800

    Update single.php

--- single.php
+++ single.php
@@ -14,9 +14,28 @@ while (have_posts()) {
 	$button_link = carbon_get_the_post_meta('button_link');
 	$bottom_text = carbon_get_the_post_meta('bottom_text');
 ?>
-	<div id="primary" class="row-fluid">
-		<div class="bg-accent"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="1757.229" height="1005.347" viewBox="0 0 1757.229 1005.347"> <defs> <clipPath id="clip-path"> <rect width="1757.229" height="1005.347" fill="none"/> </clipPath> <clipPath id="clip-path-2"> <rect id="Rectangle_361" data-name="Rectangle 361" width="1740.476" height="975.12" transform="translate(0 -0.001)" fill="none" stroke="#fab0ff" stroke-width="1"/> </clipPath> </defs> <g id="Repeat_Grid_14" data-name="Repeat Grid 14" clip-path="url(#clip-path)"> <g transform="translate(601.615 -221.887)"> <g id="Group_568" data-name="Group 568" transform="translate(1155.614 1196.857) rotate(179)" opacity="0.43"> <g id="Group_567" data-name="Group 567" clip-path="url(#clip-path-2)"> <path id="Path_2150" data-name="Path 2150" d="M1711.385,588.47c23.708,149.191-77.277,308.074-222.424,349.94s-315.236-38.824-374.626-177.723-.219-317.618,130.327-393.631C1320.7,322.783,1427.565,400,1424.4,487.928s-93.123,156.42-181.088,154.535-167.281-60.53-214.838-134.554S960.316,346,945.376,259.293C922.761,128.049,795.712,23.107,662.559,25.685S406.517,138.048,389,270.068,461.495,540.119,589.368,577.33c45.572,13.262,98.837-7.811,123-48.663s16.977-97.681-16.595-131.232C603.653,305.373,468.2,258.667,338.912,274.388s-249.6,93.53-316.97,204.986" fill="none" stroke="#fab0ff" stroke-width="51.277"/> </g> </g> </g> </g> </svg></div>
+	<div id="primary" class="row-fluid overflow-hidden">
+
 		<div id="content" role="main" class="span8 offset2">
+			<div class="bg-accent"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="1757.229" height="1005.347" viewBox="0 0 1757.229 1005.347">
+					<defs>
+						<clipPath id="clip-path">
+							<rect width="1757.229" height="1005.347" fill="none" />
+						</clipPath>
+						<clipPath id="clip-path-2">
+							<rect id="Rectangle_361" data-name="Rectangle 361" width="1740.476" height="975.12" transform="translate(0 -0.001)" fill="none" stroke="#fab0ff" stroke-width="1" />
+						</clipPath>
+					</defs>
+					<g id="Repeat_Grid_14" data-name="Repeat Grid 14" clip-path="url(#clip-path)">
+						<g transform="translate(601.615 -221.887)">
+							<g id="Group_568" data-name="Group 568" transform="translate(1155.614 1196.857) rotate(179)" opacity="0.43">
+								<g id="Group_567" data-name="Group 567" clip-path="url(#clip-path-2)">
+									<path id="Path_2150" data-name="Path 2150" d="M1711.385,588.47c23.708,149.191-77.277,308.074-222.424,349.94s-315.236-38.824-374.626-177.723-.219-317.618,130.327-393.631C1320.7,322.783,1427.565,400,1424.4,487.928s-93.123,156.42-181.088,154.535-167.281-60.53-214.838-134.554S960.316,346,945.376,259.293C922.761,128.049,795.712,23.107,662.559,25.685S406.517,138.048,389,270.068,461.495,540.119,589.368,577.33c45.572,13.262,98.837-7.811,123-48.663s16.977-97.681-16.595-131.232C603.653,305.373,468.2,258.667,338.912,274.388s-249.6,93.53-316.97,204.986" fill="none" stroke="#fab0ff" stroke-width="51.277" />
+								</g>
+							</g>
+						</g>
+					</g>
+				</svg></div>
 			<?php get_template_part('template-parts/page', 'breadcrumbs'); ?>
 			<div class="title-wrapper title-wrapper-v2">
 				<div class="container text-center">
