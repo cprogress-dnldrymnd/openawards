ebd80f3041897fa210e235e8be8212c92f685040
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-29 16:59:15 +0800

    Update ajax.php

--- includes/ajax.php
+++ includes/ajax.php
@@ -212,6 +212,6 @@ function archive_ajax()
 function hide_load_more($count, $offset, $posts_per_page)
 {
   if ($count == ($offset + $posts_per_page) || $count < ($offset + $posts_per_page) || $count < $posts_per_page + 1) {
-    return '<script>.load-more {display: block !important} </style>';
+    return '<style>.load-more {display: block !important} </style>';
   }
 }
