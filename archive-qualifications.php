6be5282e917b6ba2b0389766918f264375c310b8
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-13 20:36:30 +0800

    Update archive-qualifications.php

--- archive-qualifications.php
+++ archive-qualifications.php
@@ -54,7 +54,7 @@
                                     $levels = get_unique_meta_values('_level');
                                     ?>
                                     <select class="trigger-ajax-change" name="level" id="level">
-                                        <option value="">Level Entry</option>
+                                        <option value="">Minimum age e.g.16</option>
                                         <?php foreach ($levels as $level) { ?>
                                             <option value="<?= $level ?>"><?= $level ?></option>
                                         <?php } ?>
