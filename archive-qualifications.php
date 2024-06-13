dfed0239b598316a955c9d4f541d1998b05abecd
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-13 15:44:59 +0800

    Update archive-qualifications.php

--- archive-qualifications.php
+++ archive-qualifications.php
@@ -4,6 +4,7 @@
 <div id="primary" class="row-fluid">
     <div id="content" role="main" class="span8 offset2">
         <?php get_template_part('template-parts/page', 'breadcrumbs'); ?>
+        <?= do_shortcode('[template template_id=3722]') ?>
     </div><!-- #content .site-content -->
 </div><!-- #primary .content-area -->
 <?php get_footer(); // This fxn gets the footer.php file and renders it 
