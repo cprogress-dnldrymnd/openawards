39689bd8efd51d8b856cc7f9aba8a926a7afd3dd
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-22 10:50:40 +0800

    Update latest-news.php

--- template-parts/blog/latest-news.php
+++ template-parts/blog/latest-news.php
@@ -1,17 +1,25 @@
+<?php
+$args = array(
+    'numberposts' => 10,
+    'post_type'   => 'post'
+);
+
+$posts = get_posts($args);
+?>
+
 <section class="post-slider">
     <div class="container">
         <div class="swiperPostSlider-holder swiperPostSlider-holder-shadow position-relative swiper-button-style-1">
             <div class="swiper swiperPostSlider swiperPostSlider-latestnews bg-light">
                 <div class="swiper-wrapper">
-                    <?php while (have_posts()) {
-                        the_post(); ?>
+                    <?php foreach ($posts as $post) { ?>
                         <div class="swiper-slide">
                             <div class="post-box post-box-slider">
                                 <div class="row g-4 align-items-center">
                                     <div class="col-lg-6">
                                         <div class="column-holder">
                                             <div class="image-box">
-                                                <img src="<?= get_the_post_thumbnail_url(get_the_ID(), 'medium') ?>" alt="">
+                                                <img src="<?= get_the_post_thumbnail_url($post->ID, 'medium') ?>" alt="">
                                             </div>
                                         </div>
                                     </div>
@@ -19,13 +27,13 @@
                                         <div class="column-holder">
                                             <div class="content-box">
                                                 <div class="heading-box">
-                                                    <h4><?php the_title() ?></h4>
+                                                    <h4><?= $post->post_title ?></h4>
                                                 </div>
                                                 <div class="description-box">
-                                                    <?php the_excerpt() ?>
+                                                    <?= $post->post_excerpt ?>
                                                 </div>
                                                 <div class="button-box button-readmore">
-                                                    <a href="<?php the_permalink() ?>">Read more</a>
+                                                    <a href="<?= get_the_permalink($post->ID) ?>">Read more</a>
                                                 </div>
                                             </div>
                                         </div>
