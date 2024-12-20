95454a25a414eee759c0c19d009bbf03c0a3a261
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2024-12-20 23:21:57 +0800

    css

---
+++ single-qualifications.php
@@ -0,0 +1,37 @@
+<?php get_header() ?>
+<div id="primary" class="row-fluid">
+  <div id="content" role="main" class="span8 offset2">
+    <section class="single-qualification-hero"
+      style="background-image: url(https://openawards.theprogressteam.com/wp-content/uploads/2024/12/qual-hero-bg.png)">
+      <div class="container">
+        <div class="title-box">
+          <h1>
+            <?php the_title() ?>
+          </h1>
+        </div>
+        <div class="key-information-box">
+          <h3>Key Information</h3>
+          <div class="row">
+            <div class="col-sm-6">
+              <div class="key-info-items">
+                <div class="key-info-item">Key Information Qualification Code: 603/6198/0</div>
+                <div class="key-info-item">Sector: Vocationally-Related Qualification</div>
+                <div class="key-info-item">Level: E1</div>
+                <div class="key-info-item">Start Date: 31 July 2020</div>
+              </div>
+            </div>
+            <div class="col-sm-6">
+              <div class="key-info-items">
+                <div class="key-info-item">Review Date: 30 July 2026</div>
+                <div class="key-info-item">Guided Learning Hours: 40</div>
+                <div class="key-info-item">Min Age: Pre-16 </div>
+                <div class="key-info-item">TQT: 40</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</div>
+<?php get_footer() ?>
