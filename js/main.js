c7f2f219ae0610fa6918b5efac71950a324916ec
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2024-12-21 00:55:49 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -241,15 +241,13 @@ function ajax_qualifications($offset, $event_type = 'html') {
 
 	var $result_holder = jQuery('#results .results-holder');
 
-	var $level = jQuery("select[name='level']").val();
+	var $qualificationLevel = jQuery("select[name='qualificationLevel ']").val();
 
 	var $qcaSector = jQuery("select[name='qcaSector']").val();
 
-	var $minage = jQuery("select[name='minage']").val();
+	var $qualificationNumber = jQuery("input[name='qualificationNumber']").val();
 
-	var $code = jQuery("input[name='code']").val();
-
-	var $s = jQuery("input[name='s']").val();
+	var $qualificationTitle = jQuery("input[name='qualificationTitle']").val();
 
 	$loading = jQuery('<div class="loading-results"> <div class="spinner d-inline-block"> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--> <path d="M304 48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zm0 416a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM48 304a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm464-48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM142.9 437A48 48 0 1 0 75 369.1 48 48 0 1 0 142.9 437zm0-294.2A48 48 0 1 0 75 75a48 48 0 1 0 67.9 67.9zM369.1 437A48 48 0 1 0 437 369.1 48 48 0 1 0 369.1 437z" /> </svg> </div></div>');
 	$archive_section.addClass('loading-post');
