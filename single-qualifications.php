1f5c2f27c3f01d8744ee2b57f2af61cec0cb4ca2
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2024-12-20 23:48:42 +0800

    Update single-qualifications.php

--- single-qualifications.php
+++ single-qualifications.php
@@ -103,6 +103,15 @@
             </div>
           </div>
         </div>
+        <div class="back-to mt-4">
+          <div class="button-box-v2 button-accent">
+            <a href="#"><svg class="me-2" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                class="bi bi-chevron-left" viewBox="0 0 16 16">
+                <path fill-rule="evenodd"
+                  d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0" />
+              </svg> Back to Qualifications</a>
+          </div>
+        </div>
       </div>
     </section>
     <?= do_shortcode('[template template_id=2969]') ?>
