a3c3c5a2659e11704a76b2ab094d081f1c75b600
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-01-31 18:44:18 +0800

    Update quba.php

--- includes/quba.php
+++ includes/quba.php
@@ -306,7 +306,7 @@ function qual_grid($data, $post_type = 'qualifications', $post = false)
         '_tqt' => $data['TQT'],
         '_glh' => $data['GLH'],
         '_alternativequalificationtitle' => $data['AlternativeQualificationTitle'],
-        '_classification1' => $data['TotalCreditsRequired'],
+        '_classification1' => $data['Classification1'],
       );
       //$post_id = wp_insert_post($post_data);
     }
