cf232e7c0cad47a7963e6bc4e59cb80fb2214638
Astozel <r.ruegas123@gmail.com>
2026-09-16 16:58:51 +0800

    Responsive Related post Swiper

--- footer.php
+++ footer.php
@@ -181,6 +181,20 @@ jQuery(document).ready(function($) {
       el: ".swiper-pagination",
       clickable: true,
     },
+    breakpoints: {
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+    },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
